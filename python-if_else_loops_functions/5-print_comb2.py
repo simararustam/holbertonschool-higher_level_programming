c622f48c466c54af0581a339ff184949c6893514
simararustam <simararustam@gmail.com>
2024-05-13 11:03:20 +0000

    update

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(100):
-    end_char = '\n' if i == 99 else ', '
-    print('{:02d}{}'.format(i, end_char), end='')
+    char = '\n' if i == 99 else ', '
+    print('{:2d}{}'.format(i, char), end='')
