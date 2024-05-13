2ab0e887584626a17d1ac96641123a6df2d2e192
simararustam <simararustam@gmail.com>
2024-05-13 11:40:01 +0000

    pycodestyle

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        char = '\n' if i == 8 and j == 9 else ', ' 
+        char = '\n' if i == 8 and j == 9 else ', '
         print('{0}{1}'.format(i, j), end=char)
