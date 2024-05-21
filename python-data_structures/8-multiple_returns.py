fe6ae388ec04cc94e88d9a412e793c141c4f73b1
simararustam <simararustam@gmail.com>
2024-05-21 12:15:57 +0000

    sentence = “”

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,7 +3,8 @@ def multiple_returns(sentence):
     legth = 0
     for i in sentence:
         legth += 1
-    if sentence[0] != "":
+
+    if legth != 0:
         return legth, sentence[0]
     else:
         return legth, "None"
