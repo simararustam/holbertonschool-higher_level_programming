f55a1551521a3a5f9ce0d6f273c3d88f31585b4f
root <simararustam@gmail.com>
2024-05-24 11:21:04 +0000

    Unique addition

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    if not my_list:
+        return 0
+    
+    my_list.sort()
+    _sum = my_list[0]
+
+    for i in range(1, len(my_list)):
+        if my_list[i] != my_list[i - 1]:
+            _sum += my_list[i]
+    return _sum
