4244af87097bb31213abced9308a86ca00726b5b
root <simararustam@gmail.com>
2024-05-24 13:27:34 +0000

    Best score

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    
+    best_key = None
+    max_val = float('-inf')
+
+    for key, value in a_dictionary.items():
+        if value > max_val:
+            max_val = value
+            best_key = key
+
+    return best_key
