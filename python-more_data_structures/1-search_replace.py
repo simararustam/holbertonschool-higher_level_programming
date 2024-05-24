3e8710c42287ebf7de17e07827d4458c322d8771
root <simararustam@gmail.com>
2024-05-24 10:32:30 +0000

    update

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = my_list[:]
-    for i in range(len(new)):
+    new = my_list[i]
+    for i in range(len(new):
         if new[i] == search:
-                new[i] = replace
+            new = replace
     return new
