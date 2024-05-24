a5e74d042c3bbdbf6ad5bde1493aaf8dd1cc239e
root <simararustam@gmail.com>
2024-05-24 10:20:14 +0000

    Search and replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,9 +3,5 @@ def search_replace(my_list, search, replace):
     new = my_list[:]
     for i in range(len(new)):
         if new[i] == search:
-            for j in range(len(new)):
-                if new[j] == replace:
-                    temp = new[i]
-                    new[i] = new[j]
-                    new[j] = temp
-            return new
+                new[i] = replace
+                return new
