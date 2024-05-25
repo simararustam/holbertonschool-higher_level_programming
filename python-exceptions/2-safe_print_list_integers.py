435b98403b5de020905a9ab28ad040e8ca327cbb
simararustam <simararustam@gmail.com>
2024-05-25 14:46:26 +0000

    Pycodestyle

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,6 +8,6 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             continue
         except IndexError as e:
-            raise e  
+            raise e
     print()
     return count
