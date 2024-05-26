90068f44489cc4e37dc9c5244af1bdcb0db3d924
simararustam <simararustam@gmail.com>
2024-05-26 12:17:41 +0000

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that defines a square by: (based on 0-square.p) """
+class Square:
+    """Square Class"""
+    def __init__(self, size):
+        """Initialize the Square with the given size"""
+        self.__size = size
