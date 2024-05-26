0a593404e00b063991df6e0e978f21acc3c7ccce
simararustam <simararustam@gmail.com>
2024-05-26 13:40:20 +0000

    Access and update private attribute

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -4,29 +4,29 @@
 
 class Square:
     """An class that defines a square"""
-    
+
     def __init__(self, size=0):
         """Initialize the Square with a given size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
+
+    @property
+    def size(self):
+        """Get the current size of the square."""
+        return self.__size
 
     @size.setter
     def size(self, value):
-        """Setter method for the size attribute."""
-
+        """Set the current size of the square."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-
-    @property
-    def size(self):
-        """Getter method for the size attribute."""
-
-        return self.__size
+        self.__size = value
 
     def area(self):
-        """Calculate and return the area of the square."""
-
+        """Return the current area of the square."""
         return self.__size ** 2
