4901a7094e87547427a16e48c4322bf39b218e67
simararustam <simararustam@gmail.com>
2024-05-26 12:04:37 +0000

    Module is documented

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 class Square:
+    """ This class currently has no attributes or methods defined. """
+
+    """Example Usage:
+    --------------
+    square = Square()
+    print("Created an instance of Square:", square)
+    """
     pass
