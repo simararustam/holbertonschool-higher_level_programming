f32a6221f21ebaea9b32726824c97a0a08a568f7
simararustam <simararustam@gmail.com>
2024-05-26 13:03:54 +0000

    expected an indented block

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,12 +6,11 @@ class Square:
     """An class that defines a square"""
 
     def __init__(self, size=0):
-    """Initialize the Square with a given size."""
+        """Initialize the Square with a given size."""
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
-        
+            self.__size = size        
