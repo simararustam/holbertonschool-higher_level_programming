63608f39972ddc872baf240a13ba6fb374c44902
simararusam <simararusam@gmail.com>
2024-05-27 20:13:11 +0000

    documented

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python3
+"""This module defines the function `print_square`."""
+
+
 def print_square(size):
+    """
+    Prints a square of '#' characters with the given size.
+
+    Args:
+        size (int): The size of the square. Must be a non-negative integer.
+
+    Raises:
+        TypeError: If size is not an integer or if it is a negative float.
+        ValueError: If size is a negative integer.
+
+    Examples:
+        >>> print_square(2)
+        ##
+        ##
+    """
+
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     elif isinstance(size, float) and size < 0:
