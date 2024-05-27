26580d01588823944c23dca779081d5f8a44ffde
simararusam <simararusam@gmail.com>
2024-05-27 19:49:49 +0000

    documented

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""This module defines the `say_my_name` function."""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Print a full name composed of the first name and an optional last name.
+    """
+
     if isinstance(first_name, str) and isinstance(last_name, str):
         print("My name is {} {}".format(first_name, last_name))
     elif not isinstance(first_name, str):
