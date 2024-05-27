5949e580815ef486cb72d68799921244676323db
simararusam <simararusam@gmail.com>
2024-05-27 15:26:53 +0000

    pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,12 +5,18 @@
 def matrix_divided(matrix, div):
     """---"""
 
-    if not isinstance(matrix, list) 
-    or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if not all(len(row) == len(matrix[0]) 
-            for row in matrix):
-            raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(matrix, list):
+        raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats"
+        )
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats"
+        )
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+            raise TypeError(
+                    "Each row of the matrix must have the same size"
+            )
     new_mat = []
 
     if not isinstance(div, (int, float)):
@@ -22,7 +28,9 @@ def matrix_divided(matrix, div):
         new_row = []
         for elm in row:
             if not isinstance(elm, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(
+                        "matrix must be a matrix (list of lists) of integers/floats"
+                )
             new_row.append(round(elm / div, 2))
         new_mat.append(new_row)
         
