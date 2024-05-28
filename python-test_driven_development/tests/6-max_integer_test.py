e8d1390def1c014ede409bc74442ccba3c045237
simararustam <simararustam@gmail.com>
2024-05-28 07:24:53 +0000

    test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -6,21 +6,28 @@ max_integer = __import__("6-max_integer").max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
+    #Positive
     def positive(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+    
+    #Negative
     def negative(self):
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([-1, -3, -4, -2]), -1)
 
+    #mix element
     def mix(self):
         self.assertEqual(max_integer([-1, 2, -3, 4]), 4)
-
+        self.assertEqual(max_integer([1, -2, 3, -4]), 3)
+    
+    #empty
     def empty(self):
         self.assertIsNone(max_integer([]))
     
+    #only one elment
     def one_elm(self):
         self.asserEqual(max_integer([1]), 1)
 
 if __name__ == "__main__":
     unittest.main()
-
