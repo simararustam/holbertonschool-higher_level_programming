76c2de37fbb2bca34898a70c9b64e89bf53a9838
simararustam <simararustam@gmail.com>
2024-05-28 07:11:09 +0000

    Max integer - Unittest

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-
+"""Unittest for max_integer([..])
+"""
 import unittest
+from max_integer import max_integer
 
-max_integer = __import__("6-max_integer").max_integer
+class TestMaxInteger(unittest.TestCase):
+    def test_positive_numbers(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
-class TestMaxinteger(unittest.TestCase):
-    def positive(self):
-        self.assertEqual(max_integer([1, 3, 5, 7]), 7)
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
-    def negative(self):
-        self.assertEqual(max_integer([-1, -3, -5, -7]), -1)
+    def test_mixed_numbers(self):
+        self.assertEqual(max_integer([-1, 2, -3, 4]), 4)
 
-    def mix(self):
-        self.assertEqual(max_integer([-1, 5, 3, -7]), 5)
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]))
 
-    def one_elm(self):
-        self.assertEqual(max_integer([5]), 5)
-    
-    def empty(self):
-        self.asserEqual(max_integer([]))
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
