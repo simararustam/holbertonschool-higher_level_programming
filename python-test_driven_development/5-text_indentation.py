c95bcc5042c6d8f05209d78826468aa3f72f4ae2
simararustam <simararustam@gmail.com>
2024-05-28 05:26:20 +0000

    Correct output - case: “Holberton.School”

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -8,14 +8,17 @@ def text_indentation(text):
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+    new_text = ""
     i = 0
     while i < len(text):
         if text[i] == '.' or text[i] == '?' or text[i] == ':':
-            print()
+            new_text += text[i] + "\n"
             i += 1
         else:
             if text[i] == ' ' and i > 0 and text[i - 1] in ['.', '?', ':']:
                 i += 1
                 continue
-            print(text[i], end="")
+            new_text += text[i]
             i += 1
+
+    print(new_text)
