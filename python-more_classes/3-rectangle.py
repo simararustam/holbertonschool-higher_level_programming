34672fee334f635eece0bbc6f2c0fd729c55ca7d
simararustam <simararustam@gmail.com>
2024-05-29 09:54:10 +0000

    Write a class Rectangle that defines a rectangle by: (based on 2-rectangle.py)

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""This script defines a Rectangle class with width and height attributes."""
+
+
+class Rectangle:
+    """
+    Represents a rectangle.
+
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Returns the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculates the area of the rectangle."""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Calculates the perimeter of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
+    
+    def __str__(self):
+        _str = ""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                _str += "#"
+            _str += "\n"
+        return _str.rstrip('\n')
