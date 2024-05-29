a1734a8ef522e81cf9be73c0e3e2659b908de115
simararustam <simararustam@gmail.com>
2024-05-29 07:25:36 +0000

    pycodestyle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""This script defines a simple class Rectangle"""
+
+
 class Rectangle:
+    """
+    A simple class representing a rectangle.
+
+    Attributes:
+        No attributes defined currently.
+    """
     pass
