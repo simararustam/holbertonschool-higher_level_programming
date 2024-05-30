ed15b47fcec9fb9372d2a6c0d1e9a8837f07e740
simararustam <simararustam@gmail.com>
2024-05-30 14:05:02 +0000

    type

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry:
         self.name = name
         self.value = value
 
-        if not isinstance(value, int):
+        if not type(value) is int:
             raise TypeError("<name> must be an integer")
         if value <= 0:
             raise ValueError("<name> must be greater than 0")
