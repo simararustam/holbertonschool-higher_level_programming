ba72aa740453163b3fea810fdc420db5e0369816
simararustam <simararustam@gmail.com>
2024-05-30 12:22:09 +0000

    whitespace

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,8 +4,8 @@
 
 def is_kind_of_class(obj, a_class):
     """
-    Check if obj is an instance of, or if 
-    obj is an instance of a class that inherited 
+    Check if obj is an instance of, or if
+    obj is an instance of a class that inherited
     from,the specified class a_class.
 
     Args:
