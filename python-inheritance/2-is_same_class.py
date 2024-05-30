b18b2579e123cbcf8961d6f6af9ddc1a0e0876d7
simararustam <simararustam@gmail.com>
2024-05-30 12:09:38 +0000

    corrected

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,7 +6,4 @@ exactly an instance of the specified class ; otherwise False"""
 def is_same_class(obj, a_class):
     """Check if true or not"""
 
-    if dir(obj) == dir(a_class):
-        return True
-    else:
-        return False
+    return type(obj) is a_class
