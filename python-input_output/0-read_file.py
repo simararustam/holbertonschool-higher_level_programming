e0b6c8e4cf240d7d9dd371dfea64ba09a0f69861
simararustam <simararustam@gmail.com>
2024-06-03 06:35:59 +0000

    documented

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""This function that reads a text file (UTF8) and prints it to stdout"""
+
+
 def read_file(filename=""):
+    """
+    Reads a file line by line and prints each
+    line without adding a new line character at the end.
+
+    Parameters:
+        filename (str): The path to the file to be read.
+    """
+
     with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end="")
