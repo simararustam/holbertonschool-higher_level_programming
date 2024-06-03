974e08679b1d6fa8401acc26bdb10a0a2215b9f4
simararustam <simararustam@gmail.com>
2024-06-03 06:45:57 +0000

    Write to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""This  a function that writes a string to a text file
+(UTF8) and returns the number of characters written"""
+
+
 def write_file(filename="", text=""):
+    """Writes the specified text to a file.
+    If the file does not exist, it will be created."""
+
     with open(filename, mode='w', encoding="utf-8") as f:
         f.write(text)
     with open(filename, mode='r', encoding="utf-8") as r:
