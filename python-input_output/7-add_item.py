318688f2cc06df4b74f70f981f17efe5789f108f
simararustam <simararustam@gmail.com>
2024-06-03 09:34:27 +0000

    doc err

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Script that adds all arguments to a Python list, and then save them to a file"""
+"""Script that adds all arguments to a Python list,
+and then save them to a file"""
 import json
 import sys
 
