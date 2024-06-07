bc62b23c73d18e496d0e64f07b641bee8ceb6b04
simararustam <simararustam@gmail.com>
2024-06-07 13:04:05 +0000

    idk what is wrong

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -8,12 +8,10 @@ class CustomObject:
     A class representing a custom object with
     attributes for name, age, and student status.
     """
-
     def __init__(self, name: str, age: int, is_student: bool):
         """
         Initialize a CustomObject instance.
         """
-
         self.name = name
         self.age = age
         self.is_student = is_student
@@ -22,7 +20,6 @@ class CustomObject:
         """
         Display the attributes of the object.
         """
-
         print(f'Name: {self.name}')
         print(f'Age: {self.age}')
         print(f'Is Student: {self.is_student}')
@@ -31,7 +28,6 @@ class CustomObject:
         """
         Serialize the object to a file using pickle.
         """
-
         with open(filename, mode="wb") as file:
             pickle.dump(self, file)
 
@@ -40,6 +36,5 @@ class CustomObject:
         """
         Deserialize an object from a file using pickle.
         """
-
         with open(filename, mode="rb") as file:
             return pickle.load(file)
