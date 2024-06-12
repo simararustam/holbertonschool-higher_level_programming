4804d687635b5aeac06b2b80265d8408ddf539b9
simararustam <simararustam@gmail.com>
2024-06-12 10:04:55 +0000

    Develop a simple API using Python with the `http.server` module

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,30 @@
+import http.server
+
+class SimpleAPI(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path = '/':
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json;charset=UTF-8")
+            self.end_headers()
+            self.wfile.write("Hello, this is a simple API!")
+        elif self.path = '/data':
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json;charset=UTF-8")
+            self.end_headers()
+            data = '{"name": "John", "age": 30, "city": "New York"}'
+            self.wfile.write(data.encode())
+        elif self.path = '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'OK')
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'404 Not Found')
+
+if __name__ = "__main__":
+    serverd_adress = ('', 8000)
+    httpd = http.server.HTTPServer(server_address, SimpleAPI)
+    httpd.serve_forever()
