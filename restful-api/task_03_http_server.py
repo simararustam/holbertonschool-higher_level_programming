17e832938204108b0e17dffa10b6a10ae0100835
simararustam <simararustam@gmail.com>
2024-06-12 10:14:03 +0000

    updating 10000th time

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-
 import http.server
 
-class SimpleAPI(http.server.BaseHTTPRequestHandler):
+
+class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
+
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
-            self.send_header("Content-Type", "application/json;charset=UTF-8")
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write("Hello, this is a simple API!")
+            self.wfile.write(b'Hello, this is a simple API!')
         elif self.path == '/data':
             self.send_response(200)
-            self.send_header("Content-Type", "application/json;charset=UTF-8")
+            self.send_header('Content-type', 'application/json')
             self.end_headers()
             data = '{"name": "John", "age": 30, "city": "New York"}'
             self.wfile.write(data.encode())
@@ -26,7 +27,8 @@ class SimpleAPI(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b'404 Not Found')
 
+
 if __name__ == "__main__":
     server_address = ('', 8000)
-    httpd = http.server.HTTPServer(server_address, SimpleAPI)
+    httpd = http.server.HTTPServer(server_address, SimpleAPIHandler)
     httpd.serve_forever()
