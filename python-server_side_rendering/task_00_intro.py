159cd494c9fbad5737f91dbdbf42e220fae131f9
simararustam <simararustam@gmail.com>
2024-07-18 13:21:34 +0400

    corrected incorrectly resets template_content to template

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -4,7 +4,7 @@ def generate_invitations(template: str, attendees: list[dict]):
     if not template:
         print("Template is empty, no output files generated.")
         return
-    
+
     if not attendees:
         print("No data provided, no output files generated.")
         return
@@ -19,14 +19,14 @@ def generate_invitations(template: str, attendees: list[dict]):
     
     for index, attendee in enumerate(attendees, start=1):
         template_content = template
-        for key in ['name', 'event_date', 'event_location']:
+        for key in ['event_title', 'name', 'event_date', 'event_location']:
             placeholder = "{" + f"{key}" + "}"
             value = attendee.get(key) or "N/A"
-            template_content = template.replace(placeholder, value)
+            template_content = template_content.replace(placeholder, value)
         if not os.path.exists(f"output_{index}.txt"):
             try:
                 with open(f"output_{index}.txt", "w") as file:
                     file.write(template_content)
-                    print("Succesfully written to the {}".format(f"output_{index}.txt"))
+                    print("Successfully written to the {}".format(f"output_{index}.txt"))
             except Exception as e:
-                print("Error writen to file {}: {}".format(f"output_{index}.txt)", e))
+                print("Error writing to file {}: {}".format(f"output_{index}.txt)", e))
