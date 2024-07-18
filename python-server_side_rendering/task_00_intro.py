c363d22a6c1d9483575826546bec1a721c2f7aaa
simararustam <simararustam@gmail.com>
2024-07-18 12:20:41 +0400

    replace of content updated

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,34 +1,32 @@
 import os
 
 def generate_invitations(template: str, attendees: list[dict]):
-    if not isinstance(template, str):
-        print("Error: Template is not a string.")
-        return
-
-    if not isinstance(attendees, list) or not all(isinstance(item, dict) for item in attendees):
-        print("Error: attendees should be a list of dictionaries.")
-        return
-    
-    if template.strip() == "":
+    if not template:
         print("Template is empty, no output files generated.")
         return
     
     if not attendees:
         print("No data provided, no output files generated.")
         return
+
+    if not isinstance(template, str):
+        print("ERROR: template must be a string")
+        return
+
+    if not isinstance(attendees, list) or not all(isinstance(item, dict) for item in attendees):
+        print("Error: attendees should be a list of dictionaries.")
+        return
     
-    for index, item in enumerate(attendees, start=1):
-        for key in ['name', 'event_title', 'event_date', 'event_location']:
-            placeholder = f"{{key}}"
-            value = item.get(key)
-            if value in None:
-                value = "N/A"
-            template = template.replace(placeholder, value)
-            
-            if not os.path.exists(f"output_{index}.txt"):
-                try:
-                    with open(f"output_{index}.txt", "w") as file:
-                        file.write(template)
-                        print("Succesfully written to the {}".format(f"output_{index}.txt)"))
-                except Exception as e:
-                    print("Error writen to file {}: {}".format(f"output_{index}.txt)", e))
+    for index, attendee in enumerate(attendees, start=1):
+        template_content = template
+        for key in ['name', 'event_date', 'event_location']:
+            placeholder = "{" + f"{key}" + "}"
+            value = attendee.get(key) or "N/A"
+            template_content = template.replace(placeholder, value)
+        if not os.path.exists(f"output_{index}.txt"):
+            try:
+                with open(f"output_{index}.txt", "w") as file:
+                    file.write(template_content)
+                    print("Succesfully written to the {}".format(f"output_{index}.txt"))
+            except Exception as e:
+                print("Error writen to file {}: {}".format(f"output_{index}.txt)", e))
