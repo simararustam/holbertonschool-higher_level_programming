8fc7d953297254fc38492d1c8b81237928bf9dc1
simararustam <simararustam@gmail.com>
2024-07-26 23:34:07 +0400

    dom manipulation

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+	fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
+		.then(response => {return response.json();})
+		.then(data  => {
+			let helloElement = document.getElementById('hello');
+			let dataHello = data.hello;
+			helloElement.textContent = dataHello;
+		})
+
+		.catch(error => {
+			console.error('Error getting the hello:', error);
+		});
+});
